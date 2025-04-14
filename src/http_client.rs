e3a24c6972aa52b7b56ea0260eafb2da826bef74
Bèr Kessels <ber@berk.es>
2025-04-14 11:24:10 +0200

    feat: Add user-agent to http-client

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,6 +1,13 @@
+static APP_USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+);
+
 pub fn http_client() -> Result<reqwest::Client, reqwest::Error> {
     return reqwest::ClientBuilder::new()
             .redirect(reqwest::redirect::Policy::none())
+            .user_agent(APP_USER_AGENT)
             .connection_verbose(true)
             .build();
 }
